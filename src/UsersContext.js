5b4801c2cc70035c6d432c2c131ed483df7de696
jwkim <phoongdeng@gmail.com>
2020-12-27 23:29:37 +0900

    Refactor Users Context Reducer

--- src/UsersContext.js
+++ src/UsersContext.js
@@ -1,69 +1,29 @@
 import React, { createContext, useReducer, useContext } from 'react';
-import axios from 'axios';
+import {
+  createAsyncDispatcher,
+  createAsyncHandler,
+  initialAsyncState,
+} from './asyncActionUtils';
+import * as api from './api';
 
 const initialState = {
-  users: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  user: {
-    loading: false,
-    data: null,
-    error: null,
-  },
+  users: initialAsyncState,
+  user: initialAsyncState,
 };
 
-const loadingState = {
-  loading: true,
-  data: null,
-  error: null,
-};
-
-const success = (data) => ({
-  loading: false,
-  data,
-  error: null,
-});
-
-const error = (error) => ({
-  loading: false,
-  data: null,
-  error,
-});
+const usersHandler = createAsyncHandler('GET_USERS', 'users');
+const userHandler = createAsyncHandler('GET_USER', 'user');
 
 function reducer(state, action) {
   switch (action.type) {
     case 'GET_USERS':
-      return {
-        ...state,
-        users: loadingState,
-      };
     case 'GET_USERS_SUCCESS':
-      return {
-        ...state,
-        users: success(action.data),
-      };
     case 'GET_USERS_ERROR':
-      return {
-        ...state,
-        users: error(action.error),
-      };
+      return usersHandler(state, action);
     case 'GET_USER':
-      return {
-        ...state,
-        user: loadingState,
-      };
     case 'GET_USER_SUCCESS':
-      return {
-        ...state,
-        user: success(action.data),
-      };
     case 'GET_USER_ERROR':
-      return {
-        ...state,
-        user: error(action.error),
-      };
+      return userHandler(state, action);
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
   }
@@ -99,26 +59,5 @@ export function useUsersDispatch() {
   return dispatch;
 }
 
-export async function getUsers(dispatch) {
-  dispatch({ type: 'GET_USERS' });
-  try {
-    const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/users'
-    );
-    dispatch({ type: 'GET_USERS_SUCCESS', data: response.data });
-  } catch (error) {
-    dispatch({ type: 'GET_USERS_ERROR', error });
-  }
-}
-
-export async function getUser(dispatch, id) {
-  dispatch({ type: 'GET_USER' });
-  try {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/users/${id}`
-    );
-    dispatch({ type: 'GET_USER_SUCCESS', data: response.data });
-  } catch (error) {
-    dispatch({ type: 'GET_USER_ERROR', error });
-  }
-}
+export const getUsers = createAsyncDispatcher('GET_USERS', api.getUsers);
+export const getUser = createAsyncDispatcher('GET_USER', api.getUser);
